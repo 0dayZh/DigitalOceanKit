33076164370e7d851dd5e92371738809e9ac4cae
0dayZh <0day.zh@gmail.com>
2015-05-24 21:47:10 +0800

    Add class DOAccount.

---
+++ Pod/Classes/DOAccount.h
@@ -0,0 +1,18 @@
+//
+//  DOAccount.h
+//  Pods
+//
+//  Created by 0day on 15/5/24.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DOAccount : NSObject
+
+@property (nonatomic, assign) NSInteger dropletLimit;
+@property (nonatomic, strong) NSString  *email;
+@property (nonatomic, strong) NSString  *uuid;
+@property (nonatomic, assign, getter=isEmailVerified) BOOL      emailVerified;
+
+@end
---
+++ Pod/Classes/DOAccount.m
@@ -0,0 +1,13 @@
+//
+//  DOAccount.m
+//  Pods
+//
+//  Created by 0day on 15/5/24.
+//
+//
+
+#import "DOAccount.h"
+
+@implementation DOAccount
+
+@end
